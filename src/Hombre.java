f34ee7f4d73eca1dd71b26f2cef435e2fd624a23
davidsantiagopb <davids.prietob@konradlorenz.edu.co>
2023-09-29 15:18:24 -0500

    Quiz3

--- src/Hombre.java
+++ src/Hombre.java
@@ -1,8 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Hombres {
-    public static void main(String[] args) {
+/**
+ * Clase hombres
+ */
+public class Hombre {
+    /**
+     * atributos:
+     */
+    String nombre;
+    String frase;
+    /**
+     * Constructor de la clase
+     *@param nombre Nombre del hombre
+     *@param frase frase que hace el hombre
+     *
+     */
+    public Hombre (String nombre, String frase){
+        this.nombre=nombre;
+        this.frase=frase;
+    }
 
-        }
+    /**
+     * Imprime frase que dice el hombre
+     */
+    void decirFrase() {
+        System.out.println("El " + nombre + " dice " + frase);
+
+
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Principal {
+/**
+ * Clase que sera ejecutada
+ */
+
+public class Principal {
+    /**
+     * Primer metodo que se ejecutara en la aplicacion
+     * @param args
+     */
+    public static void main (String [] args){
+        Hombre juan = new Hombre("Juan","Me gustan todas" );
+        Hombre kamilo = new Hombre("Kamilo","Me gusta poner cachos");
+        Hombre david = new Hombre("David","Soy re calidoso");
+        Hombre esteban = new Hombre ("Esteban", "Soy muy fiel");
+
+        juan.decirFrase();
+        kamilo.decirFrase();
+        david.decirFrase();
+        esteban.decirFrase();
+    }
+
 }
